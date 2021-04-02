f5ae6e6715558c439af34b9861b2e60c8b252f07
FrancH <zhihenglu@users.noreply.github.com>
2021-04-02 11:26:09 +0800

    Test

--- Script/MolyCam.js
+++ Script/MolyCam.js
@@ -1,7 +1,7 @@
 /*转自TG
 * QuantumultX
 * ^https:\/\/pay\.wecut\.com\/apple\/iosVerifyReceipt\.php$ url script-response-body https://github.com/zhihenglu/SurgeModule/raw/master/Script/MolyCam.js
-
+* MITM = pay.wecut.com
 var obj = JSON.parse($response.body);
 var obj = {
 {

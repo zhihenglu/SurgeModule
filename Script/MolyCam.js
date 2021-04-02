899905687618aba167358a1a06a0eddb77c66c39
FrancH <zhihenglu@users.noreply.github.com>
2021-04-02 11:24:02 +0800

    Test

---
+++ Script/MolyCam.js
@@ -0,0 +1,15 @@
+/*转自TG
+* QuantumultX
+* ^https:\/\/pay\.wecut\.com\/apple\/iosVerifyReceipt\.php$ url script-response-body MolyCam.js
+
+var obj = JSON.parse($response.body);
+var obj = {
+{
+  "code": 0,
+  "msg": "处理成功",
+  "data": {
+    "isValid": 1,
+    "expiresTs": 1800000000
+  }
+}
+$done({body: JSON.stringify(obj)});

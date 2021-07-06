ee7add453a14f59bc9bf896a571568e798af8473
zhihenglu <zhihenglu@users.noreply.github.com>
2021-07-06 08:10:15 +0800

    loon_task.js

--- Loon/loon_task.js
+++ Loon/loon_task.js
@@ -7,6 +7,18 @@ cron "0 0 * * *" script-path=https://github.com/zhihenglu/SurgeModule/raw/master
 #京东试用
 cron "0 6 * * *" script-path=https://github.com/zhihenglu/SurgeModule/raw/master/Script/jd_try.js, tag=京东试用
 
+#京东
+cron "5 0 * * *" tag=京东多合一签到, script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
+
+#取关京东店铺商品
+cron "55 23 * * *" script-path=https://raw.githubusercontent.com/ChuheGit/1/main/Script/jd_scripts/jd_unsubscribe.js,tag=取关京东店铺商品
+
+#京东资产变动通知
+cron "2 9 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_bean_change.js, tag=京东资产变动通知
+
+#东东超市兑换奖品
+cron "0 0 0 * * *" script-path=https://raw.githubusercontent.com/ChuheGit/1/main/Script/jd_scripts/jd_blueCoin.js,tag=东东超市兑换奖品
+
 #一言
 cron "0 9-21/12 * * *" script-path=https://github.com/zhihenglu/SurgeModule/raw/master/Script/one_day.js, tag=一言
 

cf411a17ce273dee2b87b345aaeb9960f9771e83
zhihenglu <zhihenglu@users.noreply.github.com>
2021-07-28 09:31:08 +0800

    Script

--- Loon/loon_task.js
+++ Loon/loon_task.js
@@ -1,9 +1,6 @@
 # Task配置 By FrancH
 # Loon群组：https://t.me/Loon0x00
 
-#京喜财富岛提现
-cron "0 0 * * *" script-path=https://github.com/zhihenglu/SurgeModule/raw/master/Script/cfdtx.js, tag=京喜财富岛提现
-
 #京东试用
 cron "0 6 * * *" script-path=https://github.com/zhihenglu/SurgeModule/raw/master/Script/jd_try.js, tag=京东试用
 
@@ -16,8 +13,8 @@ cron "55 23 * * *" script-path=https://raw.githubusercontent.com/ChuheGit/1/main
 #京东资产变动通知
 cron "2 9 * * *" script-path=https://github.com/zhihenglu/SurgeModule/raw/master/Script/jd_bean_change.js, tag=京东资产变动通知
 
-#东东超市兑换奖品
-cron "0 0 0 * * *" script-path=https://raw.githubusercontent.com/ChuheGit/1/main/Script/jd_scripts/jd_blueCoin.js,tag=东东超市兑换奖品
+# 爱奇艺签到
+cron "0 9 * * *" script-path=https://raw.githubusercontent.com/NobyDa/Script/master/iQIYI-DailyBonus/iQIYI.js
 
 #一言
 cron "0 9-21/12 * * *" script-path=https://github.com/zhihenglu/SurgeModule/raw/master/Script/one_day.js, tag=一言

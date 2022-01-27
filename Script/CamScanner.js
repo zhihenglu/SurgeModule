3d5d0bf1d684f6a1eaf1b081afce92b4a1726eaf
zhihenglu <zhihenglu@users.noreply.github.com>
2022-01-27 14:59:34 +0800

    CamScanner.js

--- Script/CamScanner.js
+++ Script/CamScanner.js
@@ -12,5 +12,5 @@ QX & Q & Surge Mitm = ap*.intsig.net,
 */
 
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"4102329600"}}};
 $done({body: JSON.stringify(obj)});

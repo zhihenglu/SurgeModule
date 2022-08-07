7b7d73e32955266dc1c9fbe0a9e9fdf4250aad45
zhihenglu <57738119+zhihenglu@users.noreply.github.com>
2022-08-07 20:50:56 +0800

    QuantumultX.js

--- Script/QuantumultX.js
+++ Script/QuantumultX.js
@@ -1,4 +1,4 @@
-hostname = ios.prod.ftl.netflix.com,weather-data.apple.com, api.weather.com,passport.iqiyi.com,blindbox.jd.com,biz.caiyunapp.com,operation-api.jimistore.com,mb3admin.com,ms.jr.jd.com, me-api.jd.com,account.huami.com,api.revenuecat.com,*.bilibili.com,api.m.jd.com,nomo.dafork.com
+hostname = ios.prod.ftl.netflix.com,weather-data.apple.com, api.weather.com,passport.iqiyi.com,blindbox.jd.com,biz.caiyunapp.com,operation-api.jimistore.com,mb3admin.com,ms.jr.jd.com, me-api.jd.com,account.huami.com,*.bilibili.com,api.m.jd.com,nomo.dafork.com
 
 #Netflix评分
 ^https?://ios\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D url script-request-header https://raw.githubusercontent.com/yichahucha/surge/master/nf_rating.js
@@ -22,9 +22,6 @@ https:\/\/((weather-data\.apple)|(api.weather))\.com url script-request-header h
 
 #彩云js
 https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) url script-response-body https://raw.githubusercontent.com/githubdulong/Script/master/cytq.js
-
-#vsco
-^https:\/\/api\.revenuecat\.com\/v\d\/subscribers\/\d+$ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
 
 #京粉
 ^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) url script-response-body https://raw.githubusercontent.com/Tartarus2014/Script/master/jdjf_price.js
